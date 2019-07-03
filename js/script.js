5f7230c5e5943c6bcf177463db072f8d235d1920
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-03 17:13:12 +0200

    Add button restart, progressbar, cut dots

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  hash: true
+});
+
+//function for restart button
+var buttonRestart = document.querySelector('.button');
+
+buttonRestart.addEventListener( 'click', function(event) {
+  flkty.select(0);
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
