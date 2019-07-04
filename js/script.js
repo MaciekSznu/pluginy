c1b8216e78a61dd1c1b2000c6a31fe1ee467096b
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-04 17:41:45 +0200

    Add markers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -26,9 +28,15 @@ flkty.on( 'scroll', function( progress ) {
  window.initMap = function() {
   // The location of Gerlach
   var gerlach = {lat: 49.165274, lng: 20.133641};
+  var giewont = {lat: 49.251015, lng: 19.933488};
+  var lomnica = {lat: 49.195314, lng: 20.204392};
+  var rysy = {lat: 49.179561, lng: 20.079309};
   // The map, centered at Garlach
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom:11, center: gerlach});
+      document.getElementById('map'), {zoom:11, center: rysy});
   // The marker, positioned at Gerlach
-  var marker = new google.maps.Marker({position: gerlach, map: map});
+  var marker = new google.maps.Marker({position: gerlach, map: map, title: 'Gerlach'});
+  var markerTwo = new google.maps.Marker({position: giewont, map: map, title: 'Giewont'});
+  var markerThree = new google.maps.Marker({position: lomnica, map: map, title: 'Łomnica'});
+  var markerFour = new google.maps.Marker({position: rysy, map: map, title: 'Rysy'});
 }
