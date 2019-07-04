1993bc810db5aa5bcf03107d32faf69ca1d085f5
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-04 18:54:00 +0200

    Add map function and for loop

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ var flkty = new Flickity( elem, {
 
 //function for restart button
 var buttonRestart = document.querySelector('.button');
-
 buttonRestart.addEventListener( 'click', function(event) {
   flkty.select(0);
 });
@@ -43,3 +42,27 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+//function googlemaps
+// window.initMap = function(){
+//   var gerlach = {lat: 49.1643908, lng: 20.1248435};
+//   // The map, centered at Gerlach
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 11, center: gerlach});
+//   // The marker, positioned at Gerlach
+//   var marker = new google.maps.Marker({position: gerlach, map: map});
+// };
+window.initMap = function(){
+  var map = new google.maps.Map(
+    document.getElementById('map'), 
+    {
+      zoom: 11, center: slides[0].coords
+    });
+  
+  for(var i = 0; i < slides.length; i++){
+    marker[i] = new google.maps.Marker({
+      position: slides[i].coords,
+      map: map
+    });
+  }
+};
