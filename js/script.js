997655045f8b6d48199a5df2b5f72f01db4e3306
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-04 10:30:02 +0200

    Finish js for mustache slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//mustache function
+//żeby działało musi byc wywołane zanim się odpali flickity slider
+(function(){
+  var templateSlides = document.getElementById('carousel-template-item').innerHTML;
+  var templateCarousel = document.getElementById('carousel-template').innerHTML;
+  Mustache.parse(templateSlides);
+
+  var carouselItems = '';
+
+  for(var i = 0; i < slides.length; i++){
+    console.log(slides);
+    carouselItems += Mustache.render(templateSlides, slides[i]);
+  }
+
+  var fullCarousel = Mustache.render(templateCarousel, {slides: carouselItems});
+
+  result.insertAdjacentHTML('beforeend', fullCarousel);
+
+})();
+
 'use strict'
 
 var elem = document.querySelector('.main-carousel');
@@ -10,11 +30,11 @@ var flkty = new Flickity( elem, {
 });
 
 //function for restart button
-var buttonRestart = document.querySelector('.button');
+// var buttonRestart = document.querySelector('.button');
 
-buttonRestart.addEventListener( 'click', function(event) {
-  flkty.select(0);
-});
+// buttonRestart.addEventListener( 'click', function(event) {
+//   flkty.select(0);
+// });
 
 //function for progress bar
 var progressBar = document.querySelector('.progress-bar')
@@ -23,15 +43,3 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
-(function(){
-  var templateSlides = document.getElementById('carousel-template').innerHTML;
-  Mustache.parse(templateSlides);
-
-  var carouselItems = '';
-
-  for(var i = 0; i < slides.length; i++){
-    console.log(slides);
-    carouselItems += Mustache.render(templateSlides, slides[i]);
-  }
-})();
