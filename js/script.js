1257ccbd2d2d6819dfc58bbe0f53fc98cacd622a
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-09 06:09:21 +0200

    Add first code for stop map centering

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@
 })();
 
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true,
@@ -29,14 +29,14 @@ var flkty = new Flickity( elem, {
 
 //function for restart button
 var buttonRestart = document.querySelector('.button');
-buttonRestart.addEventListener( 'click', function(event) {
+buttonRestart.addEventListener('click', function(event) {
   flkty.select(0);
 });
 
 //function for progress bar
 var progressBar = document.querySelector('.progress-bar');
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
+flkty.on('scroll', function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
   progressBar.style.width = progress * 100 + '%';
 });
 
@@ -55,17 +55,45 @@ window.initMap = function(){
       map: map
     });
 
-    marker[i].addListener( 'click', function() {
+    marker[i].addListener('click', function(){
       flkty.select(i);
     });
   }
-  flkty.on( 'change', function( index ) {
-    map.panTo(slides[index].coords);
-    map.setZoom(10);
-    {
-    center: slides[index].coords
-    }
+  // elem.addEventListener('click', function move(){
+  //   flkty.on('change', function(index) {
+  //     map.panTo(slides[index].coords);
+  //     map.setZoom(10);
+  //     {
+  //     center: slides[index].coords
+  //     }
+  //   });
+  //   elem.removeEventListener('click', move, true); 
+  // }, true);
+  var sliderMap = document.getElementById('map');
+  var markerStopCenter = function (){
+      elem.removeEventListener('click', function(){
+        flkty.on('change', function(index) {
+          map.panTo(slides[index].coords);
+          map.setZoom(10);
+          {
+          center: slides[index].coords
+          }
+      });
+      });
+    };
+  
+  sliderMap.addEventListener('click', markerStopCenter());
+  
+  elem.addEventListener('click', function() {
+    flkty.on('change', function(index) {
+        map.panTo(slides[index].coords);
+        map.setZoom(10);
+        {
+        center: slides[index].coords
+        }
+    });
   });
+
 };
 
 
